44df7dd91168704976b076071851e48a4178f9dc
sahar118 <sahar.t118@gmail.com>
2023-03-25 01:30:28 +0300

    add files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import colors from "colors";
+// import colors from "colors";
 import morgan from "morgan";
 
 import user from './routes/userRoutes.js';
@@ -33,11 +33,11 @@ app.use('/api/v1/user', user);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
 // Handle unhandled promise rejections
 
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
+  console.log(`Error: ${err.message}`);
   server.close(() => process.exit(1));
 });
